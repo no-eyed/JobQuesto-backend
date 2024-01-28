d213623567a171e7aa13fad7010672d371c3fe17
Shobhit Arya <shobhitarya2002@gmail.com>
2024-01-29 01:30:52 +0530

    rm config.js

--- config.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-    // API_ID: process.env.APP_ID,
-    // API_KEY: process.env.API_KEY,
-    API_ID: "d4c585cf",
-    API_KEY: "9af653b3eaf71a48d621e533fa8890db",
-    BASE_URL: "https://api.adzuna.com/v1/api/jobs",
-    BASE_PARAMS: 'search/1?',                                                       
-};
